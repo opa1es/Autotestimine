27bc6b5348aa29c00c4848e822470a2bd0467605
sezareTTU <32343168+sezareTTU@users.noreply.github.com>
2017-10-11 22:56:09 +0300

    Add files via upload

---
+++ Autotestimine/src/main/java/reptositories/MainReprository.java
@@ -0,0 +1,18 @@
+package reptositories;
+
+/**
+ * Created by lll on 11-Oct-17.
+ */
+public class MainReprository{
+
+
+    private String url = "http://api.openweathermap.org/data/2.5";
+    private String key = "1fd8292118ee92959ac8793f9b0d9c1c";
+
+
+
+
+
+
+}
+
