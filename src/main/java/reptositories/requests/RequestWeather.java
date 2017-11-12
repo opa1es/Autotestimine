3e1c1b3f6a445959860dc8e82e5ce33530954662
sezare <sezare@ttu.ee>
2017-11-12 21:05:34 +0200

    works with essues. Not enouth tests

--- src/main/java/reptositories/requests/RequestWeather.java
+++ src/main/java/reptositories/requests/RequestWeather.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class RequestWeather {
 
-    JsonObjects jsonObjects;
+    private JsonObjects jsonObjects = new JsonObjects();
     private City city;
     private double temperature;
     private JSONObject forecast;
