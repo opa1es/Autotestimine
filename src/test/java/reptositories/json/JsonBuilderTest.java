a6afe2f434b9e38d6b2572bfed4c5c9823d1cde9
sezare <sezare@ttu.ee>
2017-12-16 22:06:33 +0200

    TODO:
    try closer to clean code
    
    more tests, mocking!

--- src/test/java/reptositories/json/JsonBuilderTest.java
+++ src/test/java/reptositories/json/JsonBuilderTest.java
@@ -1,18 +1,36 @@
 package reptositories.json;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reptositories.coordinatesAndCity.City;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
 
 class JsonBuilderTest {
+
+
+    City cityMock;
+    JsonBuilder jsonBuilder;
+    @BeforeEach
+    void setUP(){
+        cityMock = mock(City.class);
+        jsonBuilder = new JsonBuilder();
+    }
+
+
+
+
     @Test
-    void getRequest() {
-
-
-
-
-
-
-
-
+    void test_getForecastJSON() {
+        try {
+            assertNotNull(jsonBuilder.getRequest(cityMock));
+        } catch (IOException e) {
+            fail("It is Null...");
+        }
     }
 
 }
